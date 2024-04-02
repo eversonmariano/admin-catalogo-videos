956b7f99fffa6805ebf61b0ebd5c03564f6b07f2
Everson Mariano <mariano.computacao@gmail.com>
2024-04-02 17:19:18 -0300

    Comportamento de ativar e desativar uma categoria

--- domain/src/main/java/com/dev/admin/catalogo/domain/category/CategoryValidator.java
+++ domain/src/main/java/com/dev/admin/catalogo/domain/category/CategoryValidator.java
@@ -6,8 +6,10 @@ import com.dev.admin.catalogo.domain.validation.Validator;
 
 public class CategoryValidator extends Validator {
 
+    public static final int NAME_MAX_LENGTH = 255;
+    public static final int NAME_MIN_LENGTH = 3;
     private final Category category;
-   public CategoryValidator(final Category aCategory ,final ValidationHandler aHandler) {
+   public CategoryValidator(final Category aCategory, final ValidationHandler aHandler) {
        super(aHandler);
        this.category = aCategory;
    }
@@ -31,7 +33,7 @@ public class CategoryValidator extends Validator {
            return;
        }
        final int length = name.trim().length();
-       if (length > 255 || length <3) {
+       if (length > NAME_MAX_LENGTH || length < NAME_MIN_LENGTH) {
            this.validationHandler().append(new Error("'name' must be between 3 and 255 characters"));
        }
     }
